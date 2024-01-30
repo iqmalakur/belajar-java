7513d7efc4722b71268267e9687f98de83f0278d
iqmalakur <iakbarkurnia@gmail.com>
2024-01-30 17:48:20 +0700

    [belajar-java-unit-test] repeated test

--- belajar-java-unit-test/src/test/java/akur/belajar/test/RandomCalculatorTest.java
+++ belajar-java-unit-test/src/test/java/akur/belajar/test/RandomCalculatorTest.java
@@ -1,9 +1,7 @@
 package akur.belajar.test;
 
 import akur.belajar.test.resolver.RandomParameterResolver;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
 
@@ -15,6 +13,35 @@ import java.util.Random;
 public class RandomCalculatorTest extends ParentCalculatorTest {
     @Test
     void testRandom(Random random, TestInfo info){
+        var a = random.nextInt();
+        var b = random.nextInt();
+
+        var result = calculator.add(a, b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @DisplayName("Test random calculator")
+    @RepeatedTest(
+            value = 10,
+            name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
+    )
+    void testRandomRepeat(Random random, TestInfo info){
+        var a = random.nextInt();
+        var b = random.nextInt();
+
+        var result = calculator.add(a, b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @DisplayName("Test random calculator")
+    @RepeatedTest(value = 10, name = "{displayName}")
+    void testRandomRepeatInfo(Random random, TestInfo info, RepetitionInfo repetitionInfo){
+        System.out.printf("%s ke %s dari %s\n", info.getDisplayName(), repetitionInfo.getCurrentRepetition(), repetitionInfo.getTotalRepetitions());
+
         var a = random.nextInt();
         var b = random.nextInt();
 
