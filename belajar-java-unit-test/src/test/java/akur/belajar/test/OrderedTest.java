35352d8a677a982367817ff5c964262140b8808e
iqmalakur <iakbarkurnia@gmail.com>
2024-01-30 17:13:24 +0700

    [belajar-java-unit-test] test lifecycle

--- belajar-java-unit-test/src/test/java/akur/belajar/test/OrderedTest.java
+++ belajar-java-unit-test/src/test/java/akur/belajar/test/OrderedTest.java
@@ -1,26 +1,33 @@
 package akur.belajar.test;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+//@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // bisa menggunakan before all dan after all tanpa static
 //@TestMethodOrder(MethodOrderer.DisplayName.class)
 //@TestMethodOrder(MethodOrderer.Random.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+    private int counter = 0;
+
     @Test
     @Order(2)
     void test3() {
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(3)
     void test2() {
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(1)
     void test1() {
+        counter++;
+        System.out.println(counter);
     }
 }
