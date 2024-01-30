b6852cf5662952d0be8438a900a320022725aa7c
iqmalakur <iakbarkurnia@gmail.com>
2024-01-30 17:08:33 +0700

    [belajar-java-unit-test] test method order

---
+++ belajar-java-unit-test/src/test/java/akur/belajar/test/OrderedTest.java
@@ -0,0 +1,26 @@
+package akur.belajar.test;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+//@TestMethodOrder(MethodOrderer.DisplayName.class)
+//@TestMethodOrder(MethodOrderer.Random.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class OrderedTest {
+    @Test
+    @Order(2)
+    void test3() {
+    }
+
+    @Test
+    @Order(3)
+    void test2() {
+    }
+
+    @Test
+    @Order(1)
+    void test1() {
+    }
+}
