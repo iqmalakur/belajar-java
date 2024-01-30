5626533bc522a924a6de590e2bd659c740947396
iqmalakur <iakbarkurnia@gmail.com>
2024-01-30 09:13:24 +0700

    [belajar-java-unit-test] membuat test

---
+++ belajar-java-unit-test/src/main/java/akur/belajar/test/Calculator.java
@@ -0,0 +1,7 @@
+package akur.belajar.test;
+
+public class Calculator {
+    public Integer add(Integer first, Integer second){
+        return first + second;
+    }
+}
---
+++ belajar-java-unit-test/src/test/java/akur/belajar/test/CalculatorTest.java
@@ -0,0 +1,14 @@
+package akur.belajar.test;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAddSuccess(){
+        var result = calculator.add(10, 10);
+    }
+
+
+}
