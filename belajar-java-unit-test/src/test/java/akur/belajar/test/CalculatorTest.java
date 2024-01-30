fabf4c951fa8df811ea93c19bf4d05d4b2ed09ac
iqmalakur <iakbarkurnia@gmail.com>
2024-01-30 09:53:29 +0700

    [belajar-java-unit-test] display name

--- belajar-java-unit-test/src/test/java/akur/belajar/test/CalculatorTest.java
+++ belajar-java-unit-test/src/test/java/akur/belajar/test/CalculatorTest.java
@@ -1,12 +1,15 @@
 package akur.belajar.test;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test untuk Calculator class")
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("Test skenario sukses untuk method add(integer, integer)")
     public void testAddSuccess(){
         var result = calculator.add(10, 10);
 
