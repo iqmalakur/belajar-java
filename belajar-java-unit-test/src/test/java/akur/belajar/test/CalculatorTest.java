608c02a7792b276ecc5c07f361ce3b61d56613bc
iqmalakur <iakbarkurnia@gmail.com>
2024-01-30 09:45:56 +0700

    [belajar-java-unit-test] menggunakan assertions

--- belajar-java-unit-test/src/test/java/akur/belajar/test/CalculatorTest.java
+++ belajar-java-unit-test/src/test/java/akur/belajar/test/CalculatorTest.java
@@ -1,6 +1,7 @@
 package akur.belajar.test;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
@@ -8,7 +9,23 @@ public class CalculatorTest {
     @Test
     public void testAddSuccess(){
         var result = calculator.add(10, 10);
+
+//        if (result != 20) {
+//            throw new RuntimeException("Test gagal");
+//        }
+
+        assertEquals(20, result);
     }
 
+    @Test
+    public void testDivideSuccess(){
+        var result = calculator.divide(100, 10);
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void testDivideFailed(){
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(10, 0));
+    }
 
 }
