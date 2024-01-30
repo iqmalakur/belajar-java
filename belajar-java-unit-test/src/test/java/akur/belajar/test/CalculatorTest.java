35fbf9961496dbe9181d80f797d472a17cdec88f
iqmalakur <iakbarkurnia@gmail.com>
2024-01-30 10:11:35 +0700

    [belajar-java-unit-test] assumptions

--- belajar-java-unit-test/src/test/java/akur/belajar/test/CalculatorTest.java
+++ belajar-java-unit-test/src/test/java/akur/belajar/test/CalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*;
 import org.opentest4j.TestAbortedException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 @DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 //@DisplayName("Test untuk Calculator class")
@@ -63,6 +64,13 @@ public class CalculatorTest {
         if(!"DEV".equals(profile)){
             throw new TestAbortedException("Test dibatalkan karena bukan DEV");
         }
+
+        // unit test untuk DEV
+    }
+
+    @Test
+    public void testAssumptions(){
+        assumeTrue("DEV".equals(System.getenv("PROFILE")));
 
         // unit test untuk DEV
     }
