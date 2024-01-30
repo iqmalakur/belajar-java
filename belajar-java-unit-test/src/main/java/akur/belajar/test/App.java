c37c203431e7ff0b016a4815f78d4cd8cd694aa9
iqmalakur <iakbarkurnia@gmail.com>
2024-01-30 09:05:38 +0700

    [belajar-java-unit-test] project initialization

---
+++ belajar-java-unit-test/src/main/java/akur/belajar/test/App.java
@@ -0,0 +1,13 @@
+package akur.belajar.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
